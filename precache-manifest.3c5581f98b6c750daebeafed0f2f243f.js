106c2aa2132debce2322c92b92d4bd5fe3499ee9
greyw <greywhitt@gmail.com>
2020-12-20 19:30:14 -0600

    Updates

---
+++ precache-manifest.3c5581f98b6c750daebeafed0f2f243f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2d52df07ee8258637851c37d61b124",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "7904997a7916b2c1b978",
+    "url": "/react-portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5c1f5eff5332c9020ba8",
+    "url": "/react-portfolio/static/css/main.fb79a4de.chunk.css"
+  },
+  {
+    "revision": "7904997a7916b2c1b978",
+    "url": "/react-portfolio/static/js/2.678cf434.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/react-portfolio/static/js/2.678cf434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9388c4b27e8bc7f3c6d5",
+    "url": "/react-portfolio/static/js/3.b1882400.chunk.js"
+  },
+  {
+    "revision": "5c1f5eff5332c9020ba8",
+    "url": "/react-portfolio/static/js/main.b3ef2839.chunk.js"
+  },
+  {
+    "revision": "c6e2d4f9152e8ff06204",
+    "url": "/react-portfolio/static/js/runtime-main.61021c8d.js"
+  },
+  {
+    "revision": "188f0f16bc8ad6d6cce66c79885a7aa9",
+    "url": "/react-portfolio/static/media/budget-tracker.188f0f16.png"
+  },
+  {
+    "revision": "a6cd77b41824f6e6db46175307a6d9d4",
+    "url": "/react-portfolio/static/media/clinic-pal.a6cd77b4.jpg"
+  },
+  {
+    "revision": "28422d9e67fb218e02c2d965483f0019",
+    "url": "/react-portfolio/static/media/note-taker.28422d9e.png"
+  },
+  {
+    "revision": "8cb71d8e2bbf3f498565eb9e7cd5fd44",
+    "url": "/react-portfolio/static/media/portfolio-gen.8cb71d8e.png"
+  },
+  {
+    "revision": "a6fb7227439ea14432f2cf19bdbbe105",
+    "url": "/react-portfolio/static/media/space-dash.a6fb7227.jpg"
+  },
+  {
+    "revision": "e371e8961c11941607ec21a650e27f4c",
+    "url": "/react-portfolio/static/media/team.e371e896.png"
+  },
+  {
+    "revision": "5ec5d77a1ebbdade1b8346a9436f914e",
+    "url": "/react-portfolio/static/media/tech-blog.5ec5d77a.png"
+  },
+  {
+    "revision": "52b358cef1804b759188b1bd191c76ce",
+    "url": "/react-portfolio/static/media/weather.52b358ce.png"
+  }
+]);
