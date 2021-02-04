4cb35f613605b0a5da025bbfe8ddfb7c175ecd6a
greyw <greywhitt@gmail.com>
2021-02-04 14:09:31 -0600

    fixed resume section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,38 +1,39 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Row, Col, Container } from 'react-bootstrap';
 import { GoBrowser } from "react-icons/go";
 import { BsTerminal } from 'react-icons/bs'
 
 function Resume() {
 
-    const [frontEndSkills] = useState(['HTML','CSS','JavaScript','JQuery','React','Bootstrap','Handlebars'])
+    const [frontEndSkills] = useState(['HTML', 'CSS', 'JavaScript', 'JQuery', 'React', 'Bootstrap', 'Handlebars'])
 
-    const [backEndSkills] = useState(['APIs','Node','Express', 'MySQL, Sequelize', 'MongoDB, Mongoose', 'PWAs', 'OOP', 'MVC paradigm'])
+    const [backEndSkills] = useState(['APIs', 'Node', 'Express', 'MySQL, Sequelize', 'MongoDB, Mongoose', 'PWAs', 'OOP', 'MVC paradigm'])
 
     return (
         <Container>
             <Row className='justify-content-center skillContainer'>
                 <Col className='skills' lg={6}>
-                    <GoBrowser color="#00a6fb" size={60} />
-                    <div className='size4'>Front-end proficiencies</div>
-                    <ul className='mt-4'> 
-                    {backEndSkills.map((skill, i) => (
-                       <li key={skill + i}>
-                           {skill}
-                       </li>
-                    ))}
+
+                    <BsTerminal color="#00a6fb" size={60} />
+                    <div className='size4'>Back-end proficiencies</div>
+                    <ul className='mt-4'>
+                        {backEndSkills.map((skill, i) => (
+                            <li key={skill + i}>
+                                {skill}
+                            </li>
+                        ))}
                     </ul>
                 </Col>
 
                 <Col className='skills' lg={6}>
-                    <BsTerminal color="#00a6fb" size={60} />
-                    <div className='size4'>Back-end proficiencies</div>
+                    <GoBrowser color="#00a6fb" size={60} />
+                    <div className='size4'>Front-end proficiencies</div>
                     <ul className='mt-4'>
-                    {frontEndSkills.map((skill, i) => (
-                       <li key={skill + i}>
-                           {skill}
-                       </li>
-                    ))}
+                        {frontEndSkills.map((skill, i) => (
+                            <li key={skill + i}>
+                                {skill}
+                            </li>
+                        ))}
                     </ul>
                 </Col>
             </Row>
