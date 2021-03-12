19b219d1034ac62fa42b468e8080fe536e60d60b
greyw <greywhitt@gmail.com>
2021-03-12 15:36:52 -0600

    added resume to download

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -37,7 +37,7 @@ function Resume() {
                     </ul>
                 </Col>
             </Row>
-            <p className='mt-5'>Download my full resume <a rel="noopener noreferrer" target='_blank' href={require(`../../assets/images/example-resume.png`)} download>here</a>. (Resume is a WIP and not attached yet.)</p>
+            <p className='mt-5'>Download my full resume <a rel="noopener noreferrer" target='_blank' href={require(`../../assets/images/Grey Whittenberger's Resume.pdf`)} download>here</a>.</p>
         </Container>
     )
 }
