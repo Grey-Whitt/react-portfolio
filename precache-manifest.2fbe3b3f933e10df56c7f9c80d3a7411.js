cf471e298a47d16d92b859a2ff2e6deff7c40640
greyw <greywhitt@gmail.com>
2022-03-21 12:21:09 -0500

    Updates

---
+++ precache-manifest.2fbe3b3f933e10df56c7f9c80d3a7411.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f657093398c599867f0ae5b219c36cc",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "da2fc0bd906497a88cfb",
+    "url": "/react-portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5e87b669ec662c4f71ba",
+    "url": "/react-portfolio/static/css/main.30558a48.chunk.css"
+  },
+  {
+    "revision": "da2fc0bd906497a88cfb",
+    "url": "/react-portfolio/static/js/2.d08527cd.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/react-portfolio/static/js/2.d08527cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a874f13c16ba8f510cf9",
+    "url": "/react-portfolio/static/js/3.b11ec9b1.chunk.js"
+  },
+  {
+    "revision": "5e87b669ec662c4f71ba",
+    "url": "/react-portfolio/static/js/main.070fb0ab.chunk.js"
+  },
+  {
+    "revision": "840c97122ee96acf38ad",
+    "url": "/react-portfolio/static/js/runtime-main.fc88bb2e.js"
+  },
+  {
+    "revision": "462e7db3a5e57a450ca4084525011042",
+    "url": "/react-portfolio/static/media/Grey Whittenberger's Resume.462e7db3.pdf"
+  },
+  {
+    "revision": "188f0f16bc8ad6d6cce66c79885a7aa9",
+    "url": "/react-portfolio/static/media/budget-tracker.188f0f16.png"
+  },
+  {
+    "revision": "a6cd77b41824f6e6db46175307a6d9d4",
+    "url": "/react-portfolio/static/media/clinic-pal.a6cd77b4.jpg"
+  },
+  {
+    "revision": "dfddedf0e6d050281bec674af6b9dbe7",
+    "url": "/react-portfolio/static/media/living-real.dfddedf0.png"
+  },
+  {
+    "revision": "8ff03e14cbac57fd521bcb0f93a231c9",
+    "url": "/react-portfolio/static/media/me.8ff03e14.png"
+  },
+  {
+    "revision": "28422d9e67fb218e02c2d965483f0019",
+    "url": "/react-portfolio/static/media/note-taker.28422d9e.png"
+  },
+  {
+    "revision": "8cb71d8e2bbf3f498565eb9e7cd5fd44",
+    "url": "/react-portfolio/static/media/portfolio-gen.8cb71d8e.png"
+  },
+  {
+    "revision": "a6fb7227439ea14432f2cf19bdbbe105",
+    "url": "/react-portfolio/static/media/space-dash.a6fb7227.jpg"
+  },
+  {
+    "revision": "e371e8961c11941607ec21a650e27f4c",
+    "url": "/react-portfolio/static/media/team.e371e896.png"
+  },
+  {
+    "revision": "5ec5d77a1ebbdade1b8346a9436f914e",
+    "url": "/react-portfolio/static/media/tech-blog.5ec5d77a.png"
+  },
+  {
+    "revision": "52b358cef1804b759188b1bd191c76ce",
+    "url": "/react-portfolio/static/media/weather.52b358ce.png"
+  }
+]);
